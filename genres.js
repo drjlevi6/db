d930885008d84cfb7ded17de7ebc6c6d63a64a5f
drjlevi6 <drjlevi6@aol.com>
2021-06-03 18:29:45 -0400

    Updated allMovies.mongo-embedded.js, genres.js
    Finished embedding genres and directors

--- genres.js
+++ genres.js
@@ -1,26 +1,32 @@
 [
+// 1
 	{
-		name :'Action',
+		name: 'Action',
 		description: 'Action film is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, rescues and frantic chases.'
 	},
+// 2
 	{
-		name 'Crime': 'Films of this genre generally involve various aspects of crime and its detection.',
-		description: 'Films of this genre generally involve various aspects of crime and its detection. '
+		name: 'Crime',
+		description: 'Films of this genre generally involve various aspects of crime and its detection.'
 	},
+// 3
 	{
-		name 'Romance': 'Romance films or romance movies are romantic love stories recorded in visual media for broadcast in theaters and on TV that focus on passion, emotion, and the affectionate romantic involvement of the main characters and the journey that their love takes them through dating, courtship or marriage.',
-		description:
+		name: 'Romance',
+		description:  'Romance films or romance movies are romantic love stories recorded in visual media for broadcast in theaters and on TV that focus on passion, emotion, and the affectionate romantic involvement of the main characters and the journey that their love takes them through dating, courtship or marriage.'
 	},
+// 4
 	{
-		name 'Drama': 'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-		description:
+		name: 'Drama',
+		description: 'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
 	},
+// 5
 	{
-		name 'Comedy': 'Comedy (from the Greek: κωμῳδία, kōmōdía) is a genre of fiction consisting of discourses or works intended to be humorous or amusing by inducing laughter.',
-		description:
+		name: 'Comedy',
+		description: 'Comedy (from the Greek: κωμῳδία, kōmōdía) is a genre of fiction consisting of discourses or works intended to be humorous or amusing by inducing laughter.'
 	},
+// 6
 	{
-		name 'Documentary': 'A documentary (or documentary film) is a non-fictional motion-picture intended to "document reality, primarily for the purposes of instruction, education, or maintaining a historical record".',
-		description:
+		name: 'Documentary',
+		description: 'A documentary (or documentary film) is a non-fictional motion-picture intended to "document reality, primarily for the purposes of instruction, education, or maintaining a historical record".'
 	}
 ]
